df9f396c0dce96658bf3b41d407ac7b0d0febc8e
tradxxx <freeman.a.s@yandex.ru>
2023-04-13 13:52:57 +0300

    Model Part 1

---
+++ project-GameStore-models/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_GameStore_models.Models
+{
+    public class Game : IEntity
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+        public virtual Category? Category { get; set; }
+        public virtual ICollection<Entity_Game>? Keys_Game { get; set;}
+    }
+}
