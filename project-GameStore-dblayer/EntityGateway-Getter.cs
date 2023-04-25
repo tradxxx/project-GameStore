c77db4c5ca8617ae3b82a9dd4a255328b4f7a4b3
tradxxx <freeman.a.s@yandex.ru>
2023-04-25 10:34:10 +0300

    Getters

---
+++ project-GameStore-dblayer/EntityGateway-Getter.cs
@@ -0,0 +1,50 @@
+﻿using project_GameStore_models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_GameStore_dblayer
+{
+    public partial class EntityGateway
+    {
+        public IEnumerable<Category> GetCategories(Func<Category, bool> predicate) =>
+          Context.Categories.Where(predicate).ToArray();
+        public IEnumerable<Category> GetCategories() =>
+            GetCategories((x) => true);
+
+
+        public IEnumerable<Client> GetClients(Func<Client,bool> predicate) =>
+            Context.Clients.Where(predicate).ToArray();
+        public IEnumerable<Client> GetClients() =>
+            GetClients((x) => true);
+
+
+        public IEnumerable<Entity_Game> GetEntity_Games(Func<Entity_Game,bool> predicate) =>
+            Context.Entities.Where(predicate).ToArray();
+        public IEnumerable<Entity_Game> GetEntity_Games() =>
+            GetEntity_Games((x) => true);
+
+
+        public IEnumerable<Game> GetGames(Func<Game, bool> predicate) =>
+           Context.Games.Where(predicate).ToArray();
+        public IEnumerable<Game> GetGames() =>
+            GetGames((x) => true);
+
+
+        public IEnumerable<Order> GetOrders(Func<Order, bool> predicate) =>
+           Context.Orders.Where(predicate).ToArray();
+        public IEnumerable<Order> GetOrders() =>
+            GetOrders((x) => true);
+
+
+        public IEnumerable<Platform> GetPlatforms(Func<Platform, bool> predicate) =>
+           Context.Platforms.Where(predicate).ToArray();
+        public IEnumerable<Platform> GetPlatforms() =>
+            GetPlatforms((x) => true);
+
+
+
+    }
+}
---
+++ project-GameStore-dblayer/EntityGateway.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Client;
+using project_GameStore_models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_GameStore_dblayer
+{
+    public partial class EntityGateway
+    {
+        public ProjectManagerContext Context { get; set; }
+
+        public Guid AddOrUpdate(IEntity entity)
+        {
+
+            if (entity.Id == Guid.Empty)
+                Context.Add(entity);
+            else
+                Context.Update(entity);
+            Context.SaveChanges();
+            return entity.Id;
+
+        }
+
+        public void AddOrUpdate(params IEntity[] entities)
+        {
+            var toAdd = entities.Where(x => x.Id == Guid.Empty);
+            var toUpdate = entities.Except(toAdd);
+            Context.AddRange(toAdd);
+            Context.UpdateRange(toUpdate);
+            Context.SaveChanges();
+        }
+        public void Delete(params IEntity[] entities)
+        {
+            Context.RemoveRange(entities);
+            Context.SaveChanges();
+        }
+    }
+}
